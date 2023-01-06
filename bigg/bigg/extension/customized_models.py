e20c3d1fab80eaa3d9b8f26f165599557f0091f4
rlwilliams34 <rlwilliams34@g.ucla.edu>
2023-01-05 17:47:30 -0800

    test

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -81,7 +81,7 @@ class BiggWithEdgeLen(RecurTreeGen):
             pred_mean = params[0][0].item()
             pred_lvar = params[0][1]
             pred_var = torch.add(torch.nn.functional.softplus(pred_lvar, beta = 1), 1e-6).item()
-            edge_feats = torch.FloatTensor([[np.random.normal(pred_mean, pred_var**0.5)]])
+            edge_feats = torch.FloatTensor([[np.exp(np.random.normal(pred_mean, pred_var**0.5))]])
         else:
             ### Update log likelihood with weight prediction
             logw_obs = torch.log(edge_feats)

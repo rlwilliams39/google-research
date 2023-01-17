a422f06ec15449b77b3e9c4d3506176753105cbe
rlwilliams34 <rlwilliams34@g.ucla.edu>
2023-01-17 09:15:21 -0800

    test

--- bigg/bigg/data_process/run_syn_datagen.sh
+++ bigg/bigg/data_process/run_syn_datagen.sh
@@ -14,15 +14,19 @@
 
 #!/bin/bash
 
-g_type=Phylo
+g_type=yeast
 ordering=DFS
 
-#save_dir=../../data/$g_type-$ordering
+save_dir=../../data/$g_type-$ordering
+data_dir=../../data
 
 #if [ ! -e $save_dir ]; then
 #  mkdir -p $save_dir
 #fi
 
 python synthetic_data_gen.py \
+  -save_dir $save_dir \
   -g_type $g_type \
   -node_order $ordering \
+  -data_dir $data_dir \
+  -file_name $file_name \

e9e2bc5882e27a5471ee383c5ca813a5edefa6b7
rlwilliams34 <rlwilliams34@g.ucla.edu>
2023-01-17 08:14:18 -0800

    test

--- bigg/bigg/data_process/run_syn_datagen.sh
+++ bigg/bigg/data_process/run_syn_datagen.sh
@@ -19,9 +19,9 @@ ordering=DFS
 
 #save_dir=../../data/$g_type-$ordering
 
-if [ ! -e $save_dir ]; then
-  mkdir -p $save_dir
-fi
+#if [ ! -e $save_dir ]; then
+#  mkdir -p $save_dir
+#fi
 
 python synthetic_data_gen.py \
   -save_dir $save_dir \

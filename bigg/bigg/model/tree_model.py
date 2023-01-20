59222d01c5457c9ba720b9b00761014b8ddc921b
rlwilliams34 <rlwilliams34@g.ucla.edu>
2023-01-20 06:51:18 -0800

    Test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -707,8 +707,8 @@ class RecurTreeGen(nn.Module):
                     edge_of_lv = TreeLib.GetEdgeOf(lv)
                     edge_state = (cur_states[0][~is_nonleaf], cur_states[1][~is_nonleaf])
                     target_feats = edge_feats[edge_of_lv]
-                    weight_state = self.e2w_cell(edge_state, weight_state, tree_node.depth)
-                    weight_state = self.cell_w_update(edge_embed, weight_state, tree_node.depth)
+                    weight_state = self.e2w_cell(edge_state, weight_state, 0)
+                    weight_state = self.cell_w_update(edge_embed, weight_state, 0)
                     edge_ll, _ = self.predict_edge_feats(weight_state, target_feats)
                     ll = ll + edge_ll
                 else:

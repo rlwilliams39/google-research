31e0cc84825327a2709d95ddef7b60dfe15460f3
rlwilliams34 <rlwilliams34@g.ucla.edu>
2023-02-01 11:12:16 -0800

    gamma test

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -85,18 +85,18 @@ class BiggWithEdgeLen(RecurTreeGen):
         if edge_feats is None:
             ll = 0
             #edge_feats = None
-            pred_mean = params[0][0].item()
-            pred_lvar = params[0][1]
-            pred_var = torch.add(torch.nn.functional.softplus(pred_lvar, beta = 1), 1e-6).item()
-            pred_var = torch.nn.functional.softplus(pred_lvar, beta = 1)
-            edge_feats = torch.FloatTensor([[np.random.normal(pred_mean, pred_var**0.5)]])
-            edge_feats = torch.exp(edge_feats)
+            #pred_mean = params[0][0].item()
+            #pred_lvar = params[0][1]
+            #pred_var = torch.add(torch.nn.functional.softplus(pred_lvar, beta = 1), 1e-6).item()
+            #pred_var = torch.nn.functional.softplus(pred_lvar, beta = 1)
+            #edge_feats = torch.FloatTensor([[np.random.normal(pred_mean, pred_var**0.5)]])
+            #edge_feats = torch.exp(edge_feats)
             
             ## GAMMA DISTRIBUTION DRAW
-            #pred_a = params[0][0].item()
-            #pred_b = params[0][1].item()
-            #edge_feats = torch.FloatTensor([[np.random.gamma(pred_a, pred_b)]])
-            #edge_feats = torch.log(edge_feats)
+            pred_a = params[0][0].item()
+            pred_b = params[0][1].item()
+            edge_feats = torch.FloatTensor([[np.random.gamma(pred_a, pred_b)]])
+            edge_feats = torch.log(edge_feats)
             
         else:
             ### Update log likelihood with weight prediction
@@ -110,32 +110,32 @@ class BiggWithEdgeLen(RecurTreeGen):
             z = 1 - y
             
             ## MEAN AND VARIANCE OF LOGNORMAL
-            mean = params.gather(1, y.view(-1, 1)).squeeze()
-            lvar = params.gather(1, z.view(-1, 1)).squeeze()
-            var = torch.add(torch.nn.functional.softplus(lvar, beta = 1), 1e-6)
-            var = torch.nn.functional.softplus(lvar, beta = 1)
+            #mean = params.gather(1, y.view(-1, 1)).squeeze()
+            #lvar = params.gather(1, z.view(-1, 1)).squeeze()
+            #var = torch.add(torch.nn.functional.softplus(lvar, beta = 1), 1e-6)
+            #var = torch.nn.functional.softplus(lvar, beta = 1)
             #print(mean)
             #print(lvar)
             
             ## diff_sq = (mu - logw)^2
-            diff_sq = torch.square(torch.sub(mean, logw_obs))
+            #diff_sq = torch.square(torch.sub(mean, logw_obs))
             
             ## diff_sq2 = v^-1*diff_sq
-            diff_sq2 = torch.div(diff_sq, var)
+            #diff_sq2 = torch.div(diff_sq, var)
             
             ## log_var = log(v)
-            log_var = torch.log(var)
+            #log_var = torch.log(var)
             
             ## add to ll
-            ll = - torch.mul(log_var, 0.5) - torch.mul(diff_sq2, 0.5) - logw_obs - 0.5 * np.log(2*np.pi)
-            ll = torch.sum(ll)
+            #ll = - torch.mul(log_var, 0.5) - torch.mul(diff_sq2, 0.5) - logw_obs - 0.5 * np.log(2*np.pi)
+            #ll = torch.sum(ll)
             
             ## ALPHA AND BETA OF GAMMA
-            #a = params.gather(1, y.view(-1, 1)).squeeze()
-            #b = params.gather(1, z.view(-1, 1)).squeeze()
+            a = params.gather(1, y.view(-1, 1)).squeeze()
+            b = params.gather(1, z.view(-1, 1)).squeeze()
             
-            #ll = torch.mul(torch.sub(a, 1), logw_obs) - torch.div(edge_feats, b) - torch.mul(a, torch.log(b)) - torch.lgamma(a)
-            #ll = torch.sum(ll)
+            ll = torch.mul(torch.sub(a, 1), logw_obs) - torch.div(edge_feats, b) - torch.mul(a, torch.log(b)) - torch.lgamma(a)
+            ll = torch.sum(ll)
             
         #state_update = self.embed_edge_feats(torch.log(edge_feats)) 
         #state = self.edge_state_update(state_update, state)

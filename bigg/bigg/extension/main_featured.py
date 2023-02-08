01b2540ea45fbd1f6cfc58853c0f79fe2d359bf1
rlwilliams34 <rlwilliams34@g.ucla.edu>
2023-02-08 11:14:04 -0800

    no serialized

--- bigg/bigg/extension/main_featured.py
+++ bigg/bigg/extension/main_featured.py
@@ -241,7 +241,7 @@ if __name__ == '__main__':
     #########################################################################################################
     
     # debug_model(model, train_graphs[0], list_node_feats[0], list_edge_feats[0])
-    serialized = True
+    serialized = False
 
     optimizer = optim.Adam(model.parameters(), lr=cmd_args.learning_rate, weight_decay=1e-4)
     indices = list(range(len(train_graphs)))

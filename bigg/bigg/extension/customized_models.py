e1419c7c7ef4a838cf9292dc91bf697db51931d1
rlwilliams34 <rlwilliams34@g.ucla.edu>
2023-02-23 10:25:19 -0800

    softplus1

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -119,7 +119,7 @@ class BiggWithEdgeLen(RecurTreeGen):
         #params = torch.nn.functional.softplus(params, beta = 1)
         
         lognormal = False
-        b = 2.0
+        b = 1.0
         
         if edge_feats is None:
             ll = 0

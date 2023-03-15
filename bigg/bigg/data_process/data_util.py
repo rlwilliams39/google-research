269c3bb434a99ec01f6d6407d41c34ad9873a110
rlwilliams34 <rlwilliams34@g.ucla.edu>
2023-03-15 11:45:12 -0700

    test

--- bigg/bigg/data_process/data_util.py
+++ bigg/bigg/data_process/data_util.py
@@ -76,8 +76,9 @@ def get_graph_data(G, node_order, order_only=False):
     G = nx.relabel_nodes(G, orig_map)
     
     if node_order == 'default':
+        out_list.append(G)
+        return G
         out_list.append(apply_order(G, list(range(len(G))), order_only))
-        print(out_list)
     
     if node_order == 'by_time': 
         def order_tree(G, find_leaves= False):

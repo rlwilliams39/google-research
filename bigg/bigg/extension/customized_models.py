d4d2a902f902bf5361df9dbc0023480f73ef0a48
rlwilliams34 <rlwilliams34@g.ucla.edu>
2023-10-29 18:01:35 -0700

    fix

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -41,8 +41,8 @@ class BiggWithEdgeLen(RecurTreeGen):
 
     def embed_edge_feats(self, edge_feats, state = None):
         if state is None:
-            return self.edgelen_encodingLSTM(edge_feats)
-        return self.edgelen_encodingLSTM(edge_feats, state)[0]
+            return self.edgelen_encodingLSTM(edge_feats)[0]
+        return self.edgelen_encodingLSTM(edge_feats, state)
 
     def predict_node_feats(self, state, node_feats=None):
         """

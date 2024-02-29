da617165550f606dc34e84ffa10b401aac72aa9b
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-02-29 08:14:20 -0800

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -703,7 +703,7 @@ class RecurTreeGen(nn.Module):
                 left_feats = edge_feats[edge_idx[~is_rch]]
                 print(edge_feats)
                 print(edge_idx[~is_rch])
-                print(left_feats)
+                print("left feats: ", left_feats)
                 print(TOFU)
                 h_bot, c_bot = h_bot[left_ids[0]], c_bot[left_ids[0]]
                 h_bot, c_bot = selective_update_hc(h_bot, c_bot, left_ids[0], left_feats, None)#self.embed_edge_feats) ##########

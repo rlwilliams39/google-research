4e22d974c1b676af7f7309576590a1b4a4616b9a
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-03-07 11:24:25 -0800

    typo

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -473,7 +473,7 @@ class RecurTreeGen(nn.Module):
                         return ll, edge_embed, 1, cur_feats
                     
                     edge_embed = self.embed_edge_feats(cur_feats)
-                    return ll, (edge_embed, edge_embed), 1, 
+                    return ll, (edge_embed, edge_embed), 1, cur_feats
                 else:
                     return ll, (self.leaf_h0, self.leaf_c0), 1, None
         else:

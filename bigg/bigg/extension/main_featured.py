f56a353b8ff342f4f660fd491e60c9163142fceb
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-03-27 13:11:39 -0500

    fix

--- bigg/bigg/extension/main_featured.py
+++ bigg/bigg/extension/main_featured.py
@@ -173,7 +173,10 @@ if __name__ == '__main__':
                     fixed_edges = []
                     for e in pred_edges:
                         w = 1.0
-                        edge = (e[0], e[1], w)
+                        if e[0] < e[1]:
+                            edge = (e[0], e[1], w)
+                        else:
+                            edge = (e[1], e[0], w)
                         fixed_edges.append(edge)
                     pred_g.add_weighted_edges_from(fixed_edges)
                     gen_graphs.append(pred_g)

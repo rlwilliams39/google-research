c73260f2d5f3b6b1794f04989550c996b84ddb1d
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-03-27 15:47:57 -0500

    test

--- bigg/bigg/model/tree_clib/src/lib/struct_util.cpp
+++ bigg/bigg/model/tree_clib/src/lib/struct_util.cpp
@@ -118,7 +118,11 @@ GraphStruct::GraphStruct(int graph_id, int num_nodes, int num_edges,
             edge_list[y] = std::vector<int>();
         edge_list[y].push_back(x);
     }
-    cout << edge_list;
+    
+    using namespace std;
+    cout << edge_list
+    
+    
 
     for (auto it = edge_list.begin(); it != edge_list.end(); ++it)
         std::sort(it->second.begin(), it->second.end());

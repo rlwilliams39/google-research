fc3b1c593a3b980dcfc26b3513d881f13085686d
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-03-27 10:57:18 -0500

    Test

--- bigg/bigg/model/tree_clib/src/lib/struct_util.cpp
+++ bigg/bigg/model/tree_clib/src/lib/struct_util.cpp
@@ -95,7 +95,7 @@ GraphStruct::GraphStruct(int graph_id, int num_nodes, int num_edges,
     if (_edge_pairs == nullptr)
         return;
     int* edge_pairs = static_cast<int*>(_edge_pairs);
-    std::cout << "Thanks for viewing my code!";
+    std::cout << edge_pairs;
     for (int i = 0; i < num_edges; ++i)
     {
         int x = edge_pairs[i * 2];
